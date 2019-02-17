9220c01911537771aa4f58f2f8480aef7a7df4b4
Ken Aguilar <piq9117@gmail.com>
2019-02-17 13:21:00 -0600

    Common Module
    deleted.

--- src/Plaid/Common/Common.js
+++
@@ -1,9 +0,0 @@
-exports.handleCallbackImpl = function(left , right, f) {
-  return function(err, value) {
-    if (err) {
-      f(left(err))();
-    } else {
-      f(right(value))();
-    };
-  };
-};
