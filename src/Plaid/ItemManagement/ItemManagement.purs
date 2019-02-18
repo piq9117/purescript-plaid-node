b30d399aed0c5249ff9df67550d7d8947d8c5cc2
Ken Aguilar <piq9117@gmail.com>
2019-02-17 20:41:12 -0600

    updateWebHook

--- src/Plaid/ItemManagement/ItemManagement.purs
+++ src/Plaid/ItemManagement/ItemManagement.purs
@@ -1,6 +1,6 @@
 module Plaid.ItemManagement where
 
-import Affjax (Response)
+import Affjax (Response, ResponseFormatError(..))
 import Affjax.RequestBody (json)
 import Affjax.ResponseFormat (ResponseFormatError)
 import Data.Argonaut.Core (Json)
@@ -10,7 +10,7 @@ import Data.Maybe (Maybe(..))
 import Effect.Aff (Aff)
 import Foreign.Object (empty, insert)
 import Plaid (PlaidClient(..), plaidRequest)
-import Plaid.Types (AccessToken)
+import Plaid.Types (AccessToken, WebHook)
 import Prelude ((<<<), ($))
 
 -- | Pull the accounts associated with the Item.
@@ -26,6 +26,7 @@ getAccounts (PlaidClient { env, client_id, secret }) accessToken =
           insert "client_id" client_id <<<
           insert "access_token" accessToken $ empty
 
+-- | Pull the information about the Item.
 getItem :: PlaidClient -> AccessToken -> Aff (Response (Either ResponseFormatError Json))
 getItem (PlaidClient { env, client_id, secret }) accessToken =
   plaidRequest "/item/get" env reqBody
@@ -34,3 +35,18 @@ getItem (PlaidClient { env, client_id, secret }) accessToken =
           insert "secret" secret <<<
           insert "client_id" client_id <<<
           insert "access_token" accessToken $ empty
+
+-- | Update the webhook associated with the Item.
+updateWebHook
+  :: PlaidClient
+  -> AccessToken
+  -> WebHook
+  -> Aff (Response (Either ResponseFormatError Json))
+updateWebHook (PlaidClient { client_id, secret, env }) accessToken webhook =
+  plaidRequest "/item/webhook/update" env reqBody
+  where reqBody =
+          Just <<< json $ encodeJson <<<
+          insert "secret" secret <<<
+          insert "client_id" client_id <<<
+          insert "access_token" accessToken <<<
+          insert "webhook" webhook $ empty
--- src/Plaid/Types/Types.purs
+++ src/Plaid/Types/Types.purs
@@ -2,9 +2,10 @@ module Plaid.Types
        ( AccessToken
        , PublicToken
        , Path
+       , WebHook
        ) where
-
 
 type AccessToken = String
 type PublicToken = String
 type Path = String
+type WebHook = String
