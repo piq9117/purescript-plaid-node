c843822276c3327445972ae1af4ddbd112f41dd2
Ken Aguilar <piq9117@gmail.com>
2019-02-25 20:42:30 -0600

    get auth.

--- src/Plaid/Products/Products.purs
+++ src/Plaid/Products/Products.purs
@@ -1,28 +1,29 @@
-module Products where
+-- | Item Product Access
+module Plaid.Products where
 
 import Plaid.Types
 
 import Affjax (Response)
-import Affjax.ResponseFormat (ResponseFormatError)
 import Affjax.RequestBody (json)
+import Affjax.ResponseFormat (ResponseFormatError)
 import Data.Argonaut.Core (Json)
 import Data.Argonaut.Encode.Class (encodeJson)
 import Data.Either (Either)
 import Data.Maybe (Maybe(..))
 import Effect.Aff (Aff)
+import Foreign.Object (insert, empty)
 import Plaid (PlaidClient(..), plaidRequest)
 import Prelude ((<<<), ($))
-import Foreign.Object (insert, empty)
-  
+
+-- | Retrieve Auth Request
 getAuth
   :: PlaidClient
   -> AccessToken
   -> Aff (Response (Either ResponseFormatError Json))
 getAuth (PlaidClient { client_id, secret, env }) accessToken =
   plaidRequest "/auth/get" env reqBody
-  where reqBody =
+  where reqBody = 
           Just <<< json $ encodeJson <<<
           insert "client_id" client_id <<<
           insert "secret" secret <<<
           insert "access_token" accessToken $ empty
-
