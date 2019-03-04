cc48d0938e1b9041ab9663aa1cc383e6fabcc9fc
Ken Aguilar <piq9117@gmail.com>
2019-03-03 23:45:14 -0600

    Get Transactions
    getTransaction api abstraction added.

--- src/Plaid/Products/Products.purs
+++ src/Plaid/Products/Products.purs
@@ -9,11 +9,12 @@ import Affjax.ResponseFormat (ResponseFormatError)
 import Data.Argonaut.Core (Json, jsonEmptyObject)
 import Data.Argonaut.Encode.Class (class EncodeJson, encodeJson)
 import Data.Argonaut.Encode.Combinators ((:=), (~>))
+import Data.Date (Date)
 import Data.Either (Either)
 import Data.Maybe (Maybe(..))
 import Effect.Aff (Aff)
 import Plaid (plaidRequest)
-import Prelude (($))
+import Prelude (($), show)
 
 newtype ReqBody = ReqBody
   { client_id :: String
@@ -48,3 +49,49 @@ getAuth
 getAuth pd aToken =
   plaidRequest "/auth/get" pd.env
    (Just $ json $ encodeJson $ reqBody pd.client_id pd.secret aToken)
+
+type StartDate = Date
+type EndDate = Date
+
+newtype TransReqBody = TransReqBody
+  { client_id :: String
+  , secret :: String
+  , access_token :: String
+  , start_date :: String
+  , end_date :: String
+  }
+
+instance encodeTransReqBody :: EncodeJson TransReqBody where
+  encodeJson (TransReqBody req)
+    = "client_id" := req.client_id
+    ~> "secret" := req.secret
+    ~> "access_token" := req.access_token
+    ~> "start_date" := req.start_date
+    ~> "end_date" := req.end_date
+    ~> jsonEmptyObject
+
+transReqBody
+  :: PlaidClient
+  -> AccessToken
+  -> StartDate
+  -> EndDate
+  -> TransReqBody
+transReqBody pd aToken sDate eDate =
+  TransReqBody
+    { client_id: pd.client_id
+    , secret: pd.secret
+    , access_token: aToken
+    , start_date: (show sDate)
+    , end_date: (show eDate)
+    }
+
+-- | Retrieve Transactions Request
+getTransactions
+  :: PlaidClient
+  -> AccessToken
+  -> StartDate
+  -> EndDate
+  -> Aff (Response (Either ResponseFormatError Json))
+getTransactions pd aToken sDate eDate =
+  plaidRequest "/transactions/get" pd.env
+   (Just $ json $ encodeJson $ transReqBody pd aToken sDate eDate)
